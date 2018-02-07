79c2e2249e6412ec5395894d5215f879e9663b82
Nikita Smirnov <smirn_n@yahoo.com>
2018-02-07 22:23:59 +0300

    Fixed extern globals

--- SDL2_tutorial/Globals.h
+++ SDL2_tutorial/Globals.h
@@ -1,10 +1,9 @@
 #pragma once
+struct SDL_Window;
+struct SDL_Renderer;
 
 // The window we'll be rendering to
 extern SDL_Window* gWindow;
 
 // The window renderer
 extern SDL_Renderer* gRenderer;
-
-// Current displayed texture
-extern SDL_Texture* gTexture;
